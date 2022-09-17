aa4fb420ae691069c83267b95beeeeb826ffb4ca
SheriffKoder <sheriff.koder@hotmail.com>
2022-09-17 11:55:02 +0200

    Create new directory for Headfirst book notes, Notes about Javascript, Variables types and naming

---
+++ hfjs2.js
@@ -0,0 +1,67 @@
+/*
+JS The Definitive Guide
+JS Enlightenment
+
+C/C++/java, compiled, 
+takes code and produces a machine efficient representation of it, 
+usually optimized for runtime performance 
+scripting languages, interpreted, browser runs each line as it get it, 
+less priority to runtime performance, more towards prototyping/interactive coding, 
+flexibility, 
+
+JS 1.0 (Netscape) then Livescript/Javascript (addition ability) 1995
+JS 1.3 ECMA script 2000
+2005 google maps with JS
+JS 1.8.5 2012
+
+
+
+functions, reusable, makes code manageable
+
+
+HTML, markup text
+CSS, set of rules
+JS, statements, case sensitive
+
+
+
+
+
+
+console.log(
+*/
+
+//setTimeout (functionName, 5000);
+
+
+var dog = { name: "Rover", weight: 35 };
+//console.log(dog.weight);
+
+// (Math.PI) * X;
+
+
+/*    Variables and naming    */
+/*/////////////////////////////////////////////////////////////////////////////*/
+//var winners; //undeclared variable
+//var winners = 2(numbers); or "name"(strings) or false or true(boolean values)
+
+/*name variables beginning with a letter, _ , $ and be aware not to use reserved names(i.e keywords) */
+/* CamelCase */
+/* be consistent in choosing ' or ", 
+white space does not matter between code*/
+
+////var name = price - price * ( discount / 100 );
+// name, the variable
+//statement, the whole line
+//expression, the right side
+// = assignment
+
+//"text" + "concatinate" + StringOrNumberValueHere
+
+
+/* gives true or false */
+/*
+age < 14            
+cost >=3.99         
+animal == "bear"    
+*/

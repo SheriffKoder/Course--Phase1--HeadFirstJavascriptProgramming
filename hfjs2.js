45d006d7a224e1cae85f760b300c962ac59cdf9a
SheriffKoder <sheriff.koder@hotmail.com>
2022-09-19 15:13:45 +0200

    Make a game, where take inputs from user and check if found inside the pre-set locations 1)ask for input 2)if input not exit(x), check against pre-set locations 3)if found open a function to check if same place not hit before 4)if not hit before, increase number of successful hits, and add this location for reference

--- hfjs2.js
+++ hfjs2.js
@@ -9,6 +9,9 @@ scripting languages, interpreted, browser runs each line as it get it,
 less priority to runtime performance, more towards prototyping/interactive coding, 
 flexibility, 
 
+JS is a scripting language, with performance close to that of native compiled languages.
+JS is also used to script applications like Adobe Photoshop, OpenOffice and Google Apps, and is even used as a server-side programming language.
+
 JS 1.0 (Netscape) then Livescript/Javascript (addition ability) 1995
 JS 1.3 ECMA script 2000
 2005 google maps with JS
@@ -42,7 +45,7 @@ var dog = { name: "Rover", weight: 35 };
 
 /*    Variables and naming    */
 /*/////////////////////////////////////////////////////////////////////////////*/
-//var winners; //undeclared variable
+//var winners; //undeclared variable, value of undefined
 //var winners = 2(numbers); or "name"(strings) or false or true(boolean values)
 
 /*name variables beginning with a letter, _ , $ and be aware not to use reserved names(i.e keywords) */
@@ -56,7 +59,7 @@ white space does not matter between code*/
 // = assignment
 //expression, the right side
 
-//"text" + "concatinate" + StringOrNumberValueHere
+//"text" + "concatenate" + StringOrNumberValueHere
 
 
 /* gives true or false */
@@ -66,20 +69,25 @@ cost >=3.99
 animal == "bear"    
 */
 
+/*p56
+
 /*/////////////////////////////////////////////////////////////////////////////*/
 
-/*65
+/*
 while, if
 
 document.write ("Another scoop <br>");   //block
 alert("scoops > 5");
+= set value , == equality test
 
 */
 
 //NOTES
 //console.log(1000+"108"); //108 is a string, never gets added
 //primitive types, that is, numbers, strings and booleans.
-//put <script> in html end of body, so page can load the DOM before JS
+//HTML put <script> in html end of body, so page can load the DOM before JS
+//HTML <script type="text/javascript" >
+//a script tag with a file reference cant be used for code lines, make another script tag
 
 
 
@@ -109,4 +117,105 @@ else {
 }
 */
 
+/*56-65,65-79,79-87 18/09 30p/30p / 87:logic 
 
+/*/////////////////////////////////////////////////////////////////////////////*/
+
+/*87-91, */
+
+let hits = 0;
+let inputs = 0;
+let missout = 0;
+let ship_positions = [5,6];
+let ship_hits = [];
+let user_guess;
+//let guess_positions = Array(7);
+
+//console.log(ship_positions);
+//console.log(guess_positions.length);
+
+//let user_guess = prompt('guess from 1 to 7');
+//let user_guess = 5;
+//console.log(user_guess);
+
+hitandcheck();
+console.log("Result");
+console.log("inputs" + inputs);
+console.log("hits" + hits);
+console.log("misses" + missout);
+
+
+
+function hitandcheck ( ) { 
+    
+    while (hits < ship_positions.length && user_guess !== 'x') {
+
+
+        user_guess = prompt('guess from 1 to 7');
+        inputs++;
+
+        let miss1 = 0;
+
+        for ( i=0; i<ship_positions.length; i++ ) {
+            
+
+            if (user_guess == ship_positions[i]) {
+                checkSink(user_guess);
+
+            }
+            else {
+                console.log("not found at" + i);
+                miss1++;
+                console.log(miss1);
+            }
+
+            if (miss1 == ship_positions.length) {
+                missout++;
+            }
+
+        }
+    }
+
+
+
+
+        function checkSink (user_input) {
+
+            let all_good = 0;
+
+            for (i=0; i<ship_hits.length; i++) {
+                if (user_input !== ship_hits[i]) {
+
+                    all_good++;
+                }
+            }    
+            console.log(all_good);
+            console.log(ship_hits.length);
+
+            if (all_good == ship_hits.length) {
+
+                console.log("location exist and not hit before");
+
+                console.log("ship hits was" + ship_hits);
+                ship_hits.push(user_input);
+                console.log("ship hits now" + ship_hits);
+
+                console.log("hits was " + hits);
+                hits++;
+                console.log("hits now " + hits);
+
+
+            }
+
+            else if ( all_good !== ship_hits.length) {
+                console.log("location exist but hit before");
+
+            }
+        }
+        
+    }
+
+/*
+
+
+*/
